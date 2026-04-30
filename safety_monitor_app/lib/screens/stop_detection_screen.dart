dfc5dd1de7a78fae3074eca6dcc710f8f00b1a70
LAR <text5ffice@gmail.com>
2026-04-30 12:02:23 +0900

    Update stop_detection_screen.dart

--- safety_monitor_app/lib/screens/stop_detection_screen.dart
+++ safety_monitor_app/lib/screens/stop_detection_screen.dart
@@ -44,7 +44,7 @@ class _StopDetectionScreenState extends State<StopDetectionScreen>
     _controller = StopDetectionController(
       workerId: widget.workerId,
       workerName: widget.workerName,
-      hardwareUrl: 'http://192.168.1.100:5000', // TODO: 실제 주소로 변경
+      hardwareUrl: 'http://localhost:50243/', // TODO: 실제 주소로 변경
       pushNotifier: MockPushNotifier(), // TODO: FcmPushNotifier로 교체
       onAlertTriggered: (event) {
         setState(() {
